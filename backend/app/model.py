d81eb12e9e4691b9b2b8bd56b268a7f7fe21dbfa
kevcarrillo03 <155713837+kevcarrillo03@users.noreply.github.com>
2025-10-24 20:34:44 -0700

    created collection for user

--- backend/app/model.py
+++ backend/app/model.py
@@ -1,5 +1,8 @@
 #blueprint for database
 #pip install flask flask-mongoengine
 from . import db #imports the database from init file
+class User(db.document): #idk the database set up yet
+    email= db.StringField(required=True, unique=True)
+    username = db.StringField(required=True)
+    google_id = db.StringField() #this is just to store the unique id google gives us back
 
-#class User(db.document): #idk the database set up yet
