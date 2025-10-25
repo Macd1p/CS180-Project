0385aaa5149fd3f01b444f0cfc5bb323624787a4
kevcarrillo03 <155713837+kevcarrillo03@users.noreply.github.com>
2025-10-24 20:32:59 -0700

    changed func name to defult and registered bp aswell as fixed errors

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,12 +5,15 @@ from flask_mongoengine import MongoEngine
 
 db=MongoEngine()
 
-def our_app():
+def create_app():
     app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object('config')
+    app.config.from_pyfile('config.py',silent=True) #this just loads the configs from instance
 
-    app.config.from_pyfile('config.py') #this just loads the configs from instance
+    db.init_app(app) #this connects mongoengine to the app
 
-    db.init_app() #this connects mongoengine to the app
+    from . import auth
+    app.register_blueprint(auth.bp)
 
 
     return app
