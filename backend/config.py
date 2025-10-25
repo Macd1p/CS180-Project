8af27df787d3c89f2903b5e3eb5f22113d27ee5d
kevcarrillo03 <155713837+kevcarrillo03@users.noreply.github.com>
2025-10-24 20:32:24 -0700

    file for configs of database

---
+++ backend/config.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'dev' 
+
+# Default database settings (for local development)
+MONGODB_SETTINGS = {
+    'db': 'parking_app_db_dev',
+    'host': 'localhost',
+    'port': 27017
+}
+
+# Placeholders for Google keys
+GOOGLE_CLIENT_ID = None
+GOOGLE_CLIENT_SECRET = None
