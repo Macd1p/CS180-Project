e3b7df36ebf047267521ea61c94c2b30343b39d9
kevcarrillo03 <155713837+kevcarrillo03@users.noreply.github.com>
2025-11-05 04:03:50 -0800

    changes for running site

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*", // Apply this rule to all routes
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups", //should fix that google cross origin error
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
