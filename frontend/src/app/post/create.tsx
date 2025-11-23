d1b09c4423d0de4ce3097dfcf4a54459ce6fcf4e
stanleyx027 <sxiao027@ucr.edu>
2025-11-23 05:30:59 -0800

    Completed create form, delete modal, and update modal

---
+++ frontend/src/app/post/create.tsx
@@ -0,0 +1,199 @@
+"use client";
+import {Textarea, Field, Label, Input } from "@headlessui/react"
+import { useState } from "react";
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
+import { MdErrorOutline } from "react-icons/md";
+
+{/*Create is the Post Submission Form*/}
+const Create = () => {
+    const [title, setTitle] = useState("");
+    const [image, setImage] = useState<File|null>(null);
+    const [address, setAddress] = useState("");
+    const [description, setDescription] = useState("");
+    const [tags, setTags] = useState("");
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState(false);
+
+    const receiveTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.target.value.trim());
+    };
+
+    const receiveImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setImage(e.target.files?.[0] ?? null);
+    };
+    
+    const receiveAddress = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setAddress(e.target.value);
+    };
+
+    const receiveDescription = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setDescription(e.target.value);
+    };
+
+    const receiveTags = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTags(e.target.value)
+    }
+    
+    
+    const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setError("");
+        setSuccess(false);
+        
+        let urlImage = ""; // we will store the image's url here to be submitted along with the other post's info
+        if (image) { // upload image file to Cloudinary
+          try { // getting permission to upload the user image to Cloudinary
+            const uploadPermission = await fetch('/api/parking/generate-signature', {
+              method: 'POST',
+              credentials: "include",
+              headers: {
+                'Authorization': `Bearer ${localStorage.getItem('fms_token')}`
+              }
+            });
+            
+            if (!uploadPermission.ok) {
+              setError("You do not have permission to upload");
+            }
+
+            const { timestamp, signature, cloud_name, api_key } = await uploadPermission.json();
+            
+            // image's info that gets uploaded to Cloudinary
+            const uploadImage = new FormData();
+            uploadImage.append('file', image);
+            uploadImage.append('timestamp', timestamp);
+            uploadImage.append('signature', signature);
+            uploadImage.append('api_key', api_key);
+            
+            // Uploading the image to Cloudinary
+            const uploadResponse = await fetch(`https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`,{ 
+                method: 'POST', 
+                body: uploadImage 
+            });
+            
+            if (!uploadResponse.ok) {
+                setError("There is an error the uploading your image");
+            }
+            
+            const imageData = await uploadResponse.json();
+            urlImage = imageData.secure_url;
+            console.log(urlImage);
+
+          } catch (error) {
+            if (error instanceof Error) {
+                setError("Error: " + error.message);
+                setSuccess(false);
+            }
+            else {
+                setError("An error has occurred.");
+                setSuccess(false);
+            }
+          }
+        }
+
+         // postSubmission is the post's info to be sent
+        const postSubmission = {title, url_for_images_:urlImage, address, description, tags};
+        
+        // Posting the post submission to the backend
+        try {
+            const response = await fetch('/api/parking/spots', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Authorization': `Bearer ${localStorage.getItem('fms_token')}`
+                },
+                credentials: "include",
+                body: JSON.stringify(postSubmission)
+            });
+
+            if (!response.ok) {
+                setError("An error regarding fetching has occurred");
+            }
+
+            // Successful submission
+            const data = await response.json();
+            console.log(data);
+            setSuccess(true);
+
+            // Clear the useStates
+            setTitle("");
+            setImage(null);
+            setAddress("");
+            setDescription("");
+            setTags("");
+
+        } catch (error) { // fail to submit the form
+            if (error instanceof Error) {
+                setError("Error: " + error.message);
+                setSuccess(false);
+            }
+            else {
+                setError("An error has occurred.");
+                setSuccess(false);
+            }
+        }   
+    };
+
+    return (
+        <form onSubmit = {handleSubmit} className="ml-10">
+            {/*Create a Post Title*/}
+            <div className="font-semibold text-3xl pb-1 underline">
+                Create a Post
+            </div>
+            
+            {/*Title Field*/}
+            <Field className="flex flex-col ml-1 pb-2">
+                <Label className="font-medium text-lg">
+                    Title:
+                </Label>
+                <Input type="text" value = {title} onChange = {receiveTitle} required className = "border w-1/7 pl-1 rounded-lg hover:shadow-md" placeholder="Enter a title"/>
+            </Field>
+            
+            {/*Upload Image File Field*/}
+            <Field className="relative flex flex-col ml-1 pb-2">
+                <Label className="font-medium text-lg">
+                    Image File:
+                </Label>
+                <Input type="file" accept = "image/*" onChange = {receiveImage} className = "absolute inset-0 opacity-0 cursor-pointer"/>
+                <div className="border w-1/7 pl-1 rounded-lg cursor-pointer">
+                    <div className="text-gray-400">
+                        Upload an image
+                    </div>
+                </div>
+            </Field>
+
+            {/*Address Field*/}
+            <Field className="flex flex-col ml-1 pb-2">
+                <Label className="font-medium text-lg">
+                    Address:
+                </Label>
+                <Input type="text" value = {address} onChange = {receiveAddress} required className = "border w-1/7 pl-1 rounded-lg hover:shadow-md" placeholder="Enter a valid address"/>
+            </Field>
+
+
+            {/*Description Field*/}
+            <Field className="flex flex-col ml-1 pb-2">
+                <Label className="font-medium text-lg">
+                    Description:
+                </Label>
+                <Textarea value = {description} onChange = {receiveDescription} className = "border w-1/7 pl-1 rounded-lg hover:shadow-md" placeholder="Type a description"/>
+            </Field>
+
+            {/*Tags Field*/}
+            <Field className="flex flex-col ml-1 pb-2">
+                <Label className="font-medium text-lg">
+                    Tags:
+                </Label>
+                <Input type="text" value = {tags} onChange={receiveTags} className = "border w-1/7 pl-1 rounded-lg hover:shadow-md" placeholder="ex: #free #street"/>
+            </Field>
+
+            {/*Submission Button*/}
+            <button type = "submit" className="bg-blue-500 text-white rounded-lg w-25 mt-2 ml-1 mb-2 cursor-pointer hover:shadow hover:bg-blue-400">
+                Create Post
+            </button>
+            {/*Message for the user to know if the post was submitted*/}
+            {success ? (<div className="text-green-600 font-medium"> <IoMdCheckmarkCircleOutline/> Successful Posting!</div>) : (error && <div className="text-red-600"> <MdErrorOutline/> {error}</div>)}
+        </form>
+    );
+}
+
+export default Create;
